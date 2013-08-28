6ffe425de5a071f1076a98e0f2ecced7b7a4bc3e
Adam Strojek <adam@strojek.info>
2013-08-29 01:20:19 +0200

    New config style for tests

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -2,7 +2,7 @@ import unittest
 
 __author__ = 'astrojek'
 
-from tests.config import KEYS, USER, USERKEY
+from tests.config import APP_KEY, APP_SECRET, USER_LOGIN, USER_KEY
 
 from wykopapi import Wykop
 
@@ -10,9 +10,8 @@ from wykopapi import Wykop
 class ConnectionTest(unittest.TestCase):
 
     def testConn(self):
-        KEY, SECRET = KEYS['connection']
-
-        api = Wykop(api_key=KEY, api_secret=SECRET)
-        result = api.request(resource=['user', 'login'], login=USER, accountkey=USERKEY)
+        api = Wykop(api_key=APP_KEY, api_secret=APP_SECRET)
+        result = api.request(resource=['user', 'login'], login=USER_LOGIN, accountkey=USER_KEY)
 
         self.assertIsNotNone(result, "Not a userkey")
+
