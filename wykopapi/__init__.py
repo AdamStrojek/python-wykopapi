0699640dbd6be5c05a2acf1e56497ed52dc61a47
Adam Strojek <adam@strojek.info>
2013-08-29 00:47:47 +0200

    Added exception for API

--- wykopapi/__init__.py
+++ wykopapi/__init__.py
@@ -1,5 +1,6 @@
 from .version import __version__, __version_info__
 
 from .base import Wykop
+from .exceptions import WykopException
 
 __author__ = 'astrojek'
--- wykopapi/exceptions.py
+++ wykopapi/exceptions.py
@@ -1,1 +1,8 @@
 __author__ = 'astrojek'
+
+class WykopException(Exception):
+
+    def __init__(self, message, code):
+        super(WykopException, self).__init__(message)
+
+        self.code = code
